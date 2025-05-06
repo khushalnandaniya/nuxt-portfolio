2c692786113c7e80ba633d4aab96acff91df3be9
khushalnandaniya <nandaniyakhushal@gmail.com>
2025-05-06 20:02:46 +0530

    first commit

---
+++ components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+hellooo
+</template>
+<script setup>
+</script>
---
+++ pages/contact/index.vue
@@ -0,0 +1,11 @@
+<template>
+{{ page }}
+</template>
+
+<script setup>
+const route = useRoute();
+const page = ref();
+page.value = route.name
+console.log(route.name,"rou");
+
+</script>
